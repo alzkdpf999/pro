52c77e8565119a679c7f0063a41d1723515b8674
DaeKyung-Yang <94583777+DaeKyung-Yang@users.noreply.github.com>
2024-05-16 14:35:07 +0900

    upload WebSecurityConfig

---
+++ backend/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.secur;ity.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/register","/api/user/**").permitAll()
+                .antMatchers("/api/teacher/**").hasAuthority("ROLE_TEACHER")
+                .antMatchers("/api/student/**").hasAuthority("ROLT_STUDENT")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exceptions{
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
