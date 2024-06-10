8354a0a776db362be6593fce3ca1fb4cc5fde593
DaeKyung-Yang <94583777+DaeKyung-Yang@users.noreply.github.com>
2024-06-10 12:36:29 +0900

    Update UserController.java

--- backend/src/main/java/com/example/demo/controller/UserController.java
+++ backend/src/main/java/com/example/demo/controller/UserController.java
@@ -1,34 +1,53 @@
-//package com.example.demo.controller;
-//
-//import com.example.demo.entity.UserEntity;
-//import com.example.demo.service.UserService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api")
-//public class UserController{
-//
-//    @Autowired
-//    private UserService userService;
-//
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @PostMapping("/register")
-//    public UserEntity register(@RequestBody UserEntity user){
-//        return userService.registerUser(user);
-//    }
-//
-//    @PostMapping("/login")
-//    public String login(@RequestBody UserEntity user) {
-//        Authentication authentication = authenticationManager.authenticate(
-//            new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPasswd()));
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//        return "Login successful";
-//    }
-//}
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.entity.UserEntity;
+import com.example.demo.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController{
+
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/join")
+    public String joinPage(){
+        return "joinPage";
+    }
+    @PostMapping("/join")
+    public String joinAction(UserDto userDto){
+        userService.join(userDto);
+        return "redirect:/login";
+    }
+    @GetMapping("/login")
+    public String loginAction(){
+        return "loginPage";
+    }
+    @PostMapping("/login")
+    public String loginAction(HttpServletRequest request, UserDto userDto){
+        UserEntity user = userService.login(userDto);
+        if(user == null){
+            return "/redirect:/login";
+        }
+        return "redirect:?";
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        if(session != null){
+            session.invalidate();
+        }
+        return "redirect:/";
+    }
+}
