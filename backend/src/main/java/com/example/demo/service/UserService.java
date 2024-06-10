dedc3b6c921a595f1db3e4e33a5680571f6ea589
DaeKyung-Yang <94583777+DaeKyung-Yang@users.noreply.github.com>
2024-06-10 12:40:17 +0900

    Update UserService.java

--- backend/src/main/java/com/example/demo/service/UserService.java
+++ backend/src/main/java/com/example/demo/service/UserService.java
@@ -1,25 +1,48 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.UserDto;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.repository.JpaUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+
+    private final JpaUserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     @Autowired
-    private JpaUserRepository userRepository;
 
-    //@Autowired
-    //private PasswordEncoder passwordEncoder;
+    public UserService(JpaUserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-    public UserEntity registerUser(UserEntity user) {
-//        user.setPasswd(passwordEncoder.encode(user.getPasswd()));
-//        user.setCreateDate(new Date());
-        return userRepository.save(user);
+    public void join(UserDto userDto) {
+
+        UserEntity user = UserEntity.builder()
+                .email(userDto.getEmail())
+                .passwd(passwordEncoder.encode(userDto.getPasswd()))
+                .name(userDto.getName())
+                .build();
+
+        userRepository.save(user);
+    }
+
+    public UserEntity login(UserDto userDto) {
+        String userEmail = userDto.getEmail();
+        Optional<UserEntity> getUser = userRepository.findByUserEmail(userEmail);
+        if (getUser.isPresent()) {
+            UserEntity user = getUser.get();
+            if (user.getPasswd().equals(userDto.getPasswd())) {
+                return user;
+            }
+        }
+        return null;
     }
 }
