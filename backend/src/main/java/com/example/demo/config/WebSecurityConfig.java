80674c2ac9d9eb38dc50a923ff729bba40928c39
DaeKyung-Yang <94583777+DaeKyung-Yang@users.noreply.github.com>
2024-06-10 12:35:53 +0900

    Update WebSecurityConfig.java

--- backend/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,36 +1,44 @@
-//package com.example.demo.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//
-//@EnableWebSecurity
-//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeRequests()
-//                .antMatchers("/api/register","/api/user/**").permitAll()
-//                .antMatchers("/api/teacher/**").hasAuthority("ROLE_TEACHER")
-//                .antMatchers("/api/student/**").hasAuthority("ROLT_STUDENT")
-//                .anyRequest().authenticated()
-//                .and()
-//                .httpBasic();
-//    }
-//
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exceptions{
-//        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-//    }
-//
-//    public PasswordEncoder passwordEncoder(){
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/join","/login","/logout").permitAll()
+                        .requestMatchers("/teacher").hasRole("ROLE_TEACHER")
+                        .requestMatchers("/student").hasRole("ROLE_STUDENT")
+                        .anyRequest().authenticated()
+                );
+        http
+                .formLogin(login -> login.loginPage("/login")
+                        .defaultSuccessUrl("/")
+                );
+        http
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .invalidateHttpSession(true)
+                        .logoutSuccessUrl("/login")
+                );
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+}
