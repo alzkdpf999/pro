00e51f8ba6c585b85296affb12bc11f7fa0afb52
DaeKyung-Yang <94583777+DaeKyung-Yang@users.noreply.github.com>
2024-06-10 12:39:43 +0900

    Update JpaUserRepository.java

--- backend/src/main/java/com/example/demo/repository/JpaUserRepository.java
+++ backend/src/main/java/com/example/demo/repository/JpaUserRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.UserEntity;
 
+import java.util.Optional;
+
 public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
-  UserEntity findByEmail(String email);
+
+    Optional<UserEntity> findByUserEmail(String email);
 }
