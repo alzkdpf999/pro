e3c7db80465e362f365d2c40664ba129f926d3fb
DaeKyung-Yang <94583777+DaeKyung-Yang@users.noreply.github.com>
2024-11-14 16:06:40 +0900

    Update WebSecurityConfig.java

--- backend/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,28 +1,45 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.example.demo.repository.JpaUserRepository;
+import com.example.demo.service.CustomUserDetailsService;
+
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig{
-
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+	private final CustomUserDetailsService userService;
+	
+    @Autowired
+    public WebSecurityConfig(CustomUserDetailsService userService) {
+        this.userService = userService;
     }
-    @Bean
+
+	@Bean
+    public WebSecurityCustomizer configure(){
+        return (web) -> web.ignoring()
+                .requestMatchers("/static/**");
+    }
+	
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
 
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/join","/login","/logout").permitAll()
+                        .requestMatchers("/","/join","/login").permitAll()
                         // .requestMatchers("/teacher").hasRole("ROLE_TEACHER")
                         // .requestMatchers("/student").hasRole("ROLE_STUDENT")
                         .anyRequest().authenticated()
@@ -41,4 +58,17 @@ public class WebSecurityConfig{
         return http.build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService)
+        throws Exception{
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userService) // 사용자 정보 서비스 설정
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
